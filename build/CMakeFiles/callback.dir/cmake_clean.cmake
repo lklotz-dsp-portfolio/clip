70694901c9c9cc6e918c0a343c30a5de052e0adf
lukeklotz <lukeklotz@gmail.com>
2026-04-15 10:58:47 -0700

    playback works. writes sine and clipped sine

---
+++ build/CMakeFiles/callback.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/callback.dir/callback.c.o"
+  "CMakeFiles/callback.dir/callback.c.o.d"
+  "callback"
+  "callback.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/callback.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- callback.c
+++ callback.c
@@ -6,15 +6,19 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <math.h>
-#include <portaudio.h>
+#include </opt/homebrew/Cellar/portaudio/19.7.0/include/portaudio.h>
 
-/*
-    *
-    * Most of this code is from 
-    *     https://stackoverflow.com/questions/76059971/how-do-i-write-a-wav-file-in-c
-    * I just converted it to fit the requirements of this assignment and added some comments
-    * 
-*/
+# define SAMPLERATE 48000 
+# define AMP 0.50f
+# define FREQ 440.0f
+# define DURATION 1
+# define FRAMES DURATION * SAMPLERATE
+# define CHANNELS 1
+# define BITS 16
+# define LEN FRAMES*CHANNELS*BITS / 8
+# define SYNC 0.50
+# define MULT 32767 * SYNC
+
 
 typedef struct
 {
@@ -22,7 +26,10 @@ typedef struct
     int phaseIndex;
 } paTestData;
 
-//generate a mono sinewave and write it to a wav file. The wav file is then read and played back using PortAudio.
+void write_sine(void);
+void write_sine_clipped(void);
+
+//play a clipped sin wave 
 static int PaCallback(const void *inputBuffer, void *outputBuffer,
                       unsigned long framesPerBuffer,
                       const PaStreamCallbackTimeInfo* timeInfo,
@@ -30,16 +37,22 @@ static int PaCallback(const void *inputBuffer, void *outputBuffer,
                       void *userData)
 {
     paTestData *data = (paTestData*)userData;
-    float *out = (float*)outputBuffer;
+    int16_t *out = (int16_t*)outputBuffer;
     unsigned int i;
     (void) inputBuffer;
     for (i=0; i<framesPerBuffer; i++)
     {
         //*out++ = data->sine[data->index]; 
         //generate a sine wave on the fly
-        *out++ = (sinf(2.0f * M_PI * 440.0f * data->phaseIndex/ 48000.0f) * 0.1f);; 
+        int16_t sample = (int16_t)(sinf(2.0f * M_PI * FREQ * data->phaseIndex/ SAMPLERATE) * MULT);
+ 
+        //clip
+        if (sample >= 8196) sample = 8196;
+        if (sample <= -8196) sample = -8196;
 
-        if (data->phaseIndex >= 48000) // reset phase index after 1 second
+        *out++ = sample;
+
+        if (data->phaseIndex >= SAMPLERATE) // reset phase index after 1 second
             data->phaseIndex = 0;
         data->phaseIndex++;
     }
@@ -59,8 +72,8 @@ int main()
     err = Pa_OpenDefaultStream( &stream,
                       0,          /* no input channels */
                       1,          /* mono output */
-                      paFloat32,  /* 32 bit floating point output */
-                      48000,
+                      paInt16,  
+                      SAMPLERATE,
                       256,        /* frames per buffer */
                       PaCallback,
                       &data );
@@ -72,8 +85,123 @@ int main()
   
     err = Pa_StopStream( stream );
 
+    write_sine();
+    write_sine_clipped();
+
     return 0;
 }
 
-//compile with: gcc clip.c -o clip -lm
-// -lm is needed to link the math library, which contains the sinf function used to generate the sine wave. 
+//generate 1 second sine and write to sine.wav
+void write_sine(void){
+
+    int16_t channels = CHANNELS;
+    int32_t sampleRate = SAMPLERATE;
+    int16_t bitsPerSample = BITS;
+    int32_t len = FRAMES*CHANNELS*BITS / 8;
+    int16_t byte;
+
+
+    int16_t *channel = (int16_t *) malloc(FRAMES * sizeof(int16_t));
+    for (int i=0; i < FRAMES; i++) {
+        // generate a sine wave
+        channel[i] = (int16_t)(sinf(2.0 * M_PI * FREQ * data.phaseIndex / SAMPLERATE ) * MULT);
+
+        if(data.phaseIndex >= SAMPLERATE) data.phaseIndex = 0;
+        data.phaseIndex++;
+   }
+
+    // Writes data to wav file
+    FILE *fp = fopen("sine.wav", "wb"); // Open the file in binary mode for writing
+    //
+    //// WAVE Header Data
+    fwrite("RIFF", 1, 4, fp);
+    int32_t chunk_size = LEN + 36; 
+    fwrite(&chunk_size, 4, 1, fp);
+    fwrite("WAVE", 1, 4, fp);
+    fwrite("fmt ", 1, 4, fp);
+    int32_t subchunk1_size = 16;
+    fwrite(&subchunk1_size, 4, 1, fp);
+    int16_t fmt_type = 1;  // 1 = PCM
+    fwrite(&fmt_type, 2, 1, fp);
+    fwrite(&channels, 2, 1, fp);
+    fwrite(&sampleRate, 4, 1, fp);
+    // (Sample Rate * BitsPerSample * Channels) / 8
+    int32_t byte_rate = SAMPLERATE * BITS * CHANNELS / 8;
+    fwrite(&byte_rate, 4, 1, fp);
+    int16_t block_align = CHANNELS * BITS / 8;
+    fwrite(&block_align, 2, 1, fp);
+    fwrite(&bitsPerSample, 2, 1, fp);
+
+    // Marks the start of the data
+    fwrite("data", 1, 4, fp);
+    fwrite(&len, 4, 1, fp);  // Data size
+    
+    for (int i = 0; i < FRAMES; i++)
+    {
+        byte = channel[i];
+        fwrite(&byte, 2, 1, fp);
+    }
+
+    fclose(fp);
+    free(channel); 
+}
+
+//generate 1 second clipped sine and write to clipped.wav
+void write_sine_clipped(void){
+
+    int16_t channels = CHANNELS;
+    int32_t sampleRate = SAMPLERATE;
+    int16_t bitsPerSample = BITS;
+    int32_t len = FRAMES*CHANNELS*BITS / 8;
+    int16_t byte;
+
+
+    int16_t *channel = (int16_t *) malloc(FRAMES * sizeof(int16_t));
+    for (int i=0; i < FRAMES; i++) {
+        // generate a sine wave
+        channel[i] = (int16_t)(sinf(2.0 * M_PI * FREQ * data.phaseIndex / SAMPLERATE ) * MULT);
+
+        if(channel[i] >= 8196) channel[i] = 8196;
+        if(channel[i] <= -8186) channel[i] = -8196;
+
+        if(data.phaseIndex >= SAMPLERATE) data.phaseIndex = 0;
+        data.phaseIndex++;
+   }
+
+    // Writes data to wav file
+    FILE *fp = fopen("clipped.wav", "wb"); // Open the file in binary mode for writing
+    //
+    //// WAVE Header Data
+    fwrite("RIFF", 1, 4, fp);
+    int32_t chunk_size = LEN + 36; 
+    fwrite(&chunk_size, 4, 1, fp);
+    fwrite("WAVE", 1, 4, fp);
+    fwrite("fmt ", 1, 4, fp);
+    int32_t subchunk1_size = 16;
+    fwrite(&subchunk1_size, 4, 1, fp);
+    int16_t fmt_type = 1;  // 1 = PCM
+    fwrite(&fmt_type, 2, 1, fp);
+    fwrite(&channels, 2, 1, fp);
+    fwrite(&sampleRate, 4, 1, fp);
+    // (Sample Rate * BitsPerSample * Channels) / 8
+    int32_t byte_rate = SAMPLERATE * BITS * CHANNELS / 8;
+    fwrite(&byte_rate, 4, 1, fp);
+    int16_t block_align = CHANNELS * BITS / 8;
+    fwrite(&block_align, 2, 1, fp);
+    fwrite(&bitsPerSample, 2, 1, fp);
+
+    // Marks the start of the data
+    fwrite("data", 1, 4, fp);
+    fwrite(&len, 4, 1, fp);  // Data size
+    
+    for (int i = 0; i < FRAMES; i++)
+    {
+        byte = channel[i];
+        fwrite(&byte, 2, 1, fp);
+    }
+
+    fclose(fp);
+    free(channel); 
+}
+
+
